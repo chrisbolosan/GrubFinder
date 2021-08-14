e712c4bfd60368ad2ff6aa67c89d1cac3e87fd0c
chrisbolosan <cbolosan@gmail.com>
2021-08-14 15:54:21 -0500

    flatlistrender

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { StatusBar, SafeAreaView, FlatList } from "react-native";
 import styled from "styled-components/native";
+import { Spacer } from "../components/spacer/spacer.component";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
@@ -14,18 +15,44 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
-
+// return (
+//   <SafeAreaView style={styles.container}>
+//     <FlatList
+//       data={DATA}
+//       renderItem={renderItem}
+//       keyExtractor={item => item.id}
+//     />
+//   </SafeAreaView>
+// );
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
+    <FlatList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+      contentContainerStyle={{ padding: 16 }}
+    />
   </SafeArea>
 );
