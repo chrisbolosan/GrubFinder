fd0f974da6684635d08bc1e860b6762e3278a7fe
chrisbolosan <cbolosan@gmail.com>
2021-08-14 10:10:45 -0500

    refactored themes in components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
@@ -9,22 +8,22 @@ const RestaurantCard = styled(Card)`
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
+  padding: ${(props) => props.theme.space[3]};
   background-color: white;
 `;
 
 const Title = styled(Text)`
-  padding: 16px;
-  color: red;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = 'Thai Bistro',
+    name = "Thai Bistro",
     icon,
     photos = [
-      'http://samuiholiday.com/wp-content/uploads/2016/06/jahnmain.jpg',
+      "http://samuiholiday.com/wp-content/uploads/2016/06/jahnmain.jpg",
     ],
-    address = '100 some random street',
+    address = "100 some random street",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily,
