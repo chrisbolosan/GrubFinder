d08fe7f3922efbc146bc5e226d6049badad60310
chrisbolosan <cbolosan@gmail.com>
2021-10-18 17:38:04 -0500

    restaurant data response is transformed with camelize using fake api

--- src/services/restaurant/restaurants.service.js
+++ src/services/restaurant/restaurants.service.js
@@ -12,10 +12,22 @@ return new Promise((resolve, reject) => {
         resolve(mock)
 })
 }
-const restaurantsTransform = (result) => {
-    const newResult = camelize(result)
-    return newResult
-}
+const restaurantsTransform = ({results =[]}) => {
+    const mappedResults = results.map((restaurant) => {
+        return {
+            ...restaurant,
+            isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+            isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+            
+        }
+    });
+    return camelize(mappedResults)
+
+    
+    // const newResult = camelize(results.length)
+  
+}   
+
 restaurantsRequest ()
 .then(restaurantsTransform)
 .then((transformedResponse => {
