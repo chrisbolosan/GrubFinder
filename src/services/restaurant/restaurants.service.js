8844b97e3874849ebba9d027889a872079d1f182
chrisbolosan <cbolosan@gmail.com>
2021-10-18 16:56:17 -0500

    fake api request up

--- src/services/restaurant/restaurants.service.js
+++ src/services/restaurant/restaurants.service.js
@@ -1,7 +1,19 @@
 import {mocks} from './mock'
 
 
-export const restaurantsRequest = (location ="37.7749295,-122.4194155") => {
-console.log(mocks[location])
+export const restaurantsRequest = (location ="37.7749295,-122.4dfs194155") => {
+// console.log(mocks[location])
+return new Promise((resolve, reject) => {
+    const mock = mocks[location]
+        if(!mock) {
+            reject("Not Found")
+        } 
+        resolve(mock)
+})
 }
-restaurantsRequest ()
+restaurantsRequest ().then((result) => {
+    console.log('results $$$',result)
+})
+.catch((error) => {
+    console.log(error)
+})
