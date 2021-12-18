eaa034ab1ba4b976587b203c86f1af0a6d0f4600
chrisbolosan <cbolosan@gmail.com>
2021-12-18 17:18:30 -0600

    removed stylesheet, added searchbar and some restyling

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,22 +1,25 @@
 import React from "react";
 import MapView from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import styled from "styled-components";
+import { Search } from "../components/search.component";
 
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
 export const MapScreen = () => (
-  <View style={styles.container}>
-    <MapView style={styles.map} />
-  </View>
+  <>
+    <Search />
+    <Map />
+  </>
 );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
