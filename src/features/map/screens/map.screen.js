8968e9b102b373a687bfc95c00326de70b073a48
chrisbolosan <cbolosan@gmail.com>
2021-12-18 19:20:03 -0600

    grabbing context infor into map, setting up regions

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurant/restaurants.context";
 import { Search } from "../components/search.component";
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
-export const MapScreen = () => (
-  <>
-    <Search />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    const latDelta = northeastLat - southwestLat;
+    setLatDelta(latDelta);
+  }, [location, viewport]);
+  return (
+    <>
+      <Search />
+      <Map>
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
+    </>
+  );
+};
