968b39d8317765bd2487b75b142507dc3808fa4c
chrisbolosan <cbolosan@gmail.com>
2021-12-21 18:53:58 -0600

    restaurant photo now also renders in markers for android

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -5,6 +5,7 @@ import styled from "styled-components/native";
 const CustomMarker = styled.Text`
   height: 100%;
   width: 100%;
+  align-items: center;
 `;
 // const IconImage = styled.Image`
 //   border-radius: 10px;
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 // import { View, Text } from "react-native";
 import MapView from "react-native-maps";
-import styled from "styled-components";
+import styled from "styled-components/native";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurant/restaurants.context";
 import { CustomCallout } from "../components/map-callout.component";
 import { Search } from "../components/search.component";
+import { WebView } from "react-native-webview";
+import { Platform } from "react-native";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -13,9 +15,19 @@ const Map = styled(MapView)`
 `;
 const IconImage = styled.Image`
   border-radius: 10px;
-  height: 150px;
-  width: 150px;
+  height: 120px;
+  width: 120px;
+  align-items: center;
 `;
+
+const AndroidImage = styled(WebView)`
+  border-radius: 10px;
+  height: 120px;
+  width: 120px;
+`;
+
+const isAndroid = Platform.OS === "android";
+const FoodImage = isAndroid ? AndroidImage : IconImage;
 
 export const MapScreen = () => {
   const { location } = useContext(LocationContext);
@@ -54,7 +66,7 @@ export const MapScreen = () => {
               }}
             >
               <MapView.Callout>
-                <IconImage source={{ uri: restaurant.photos[0] }} />
+                <FoodImage source={{ uri: restaurant.photos[0] }} />
                 <CustomCallout restaurant={restaurant.name}>
                   {restaurant}
                 </CustomCallout>
