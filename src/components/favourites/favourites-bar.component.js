9c967a76c3c27d1c2a86bc39903a7e7171ab244a
chrisbolosan <cbolosan@gmail.com>
2021-12-22 19:15:36 -0600

    setting up horizontal way

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -1,5 +1,18 @@
 import React from "react";
-import { Text } from "react-native";
-export const FavouritesBar = () => {
-  return <Text>I'm a favourites bar</Text>;
+import styled from "styled-components/native";
+import { ScrollView } from "react-native-gesture-handler";
+
+const FavouritesWrapper = styled.View`
+  padding: 10px;
+  height: 35px;
+  width: 100%;
+
+  border-radius: 5px;
+`;
+export const FavouritesBar = ({ favourites }) => {
+  return (
+    <FavouritesWrapper>
+      <ScrollView horizontal showsHorizontalScrollIndicator={true}></ScrollView>
+    </FavouritesWrapper>
+  );
 };
