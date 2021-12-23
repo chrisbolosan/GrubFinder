61f782f0c46320dcd2192cad0caf079bd1c0e241
chrisbolosan <cbolosan@gmail.com>
2021-12-23 16:55:19 -0600

    render bug

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -4,30 +4,40 @@ import styled from "styled-components/native";
 import { ScrollView } from "react-native-gesture-handler";
 import { Spacer } from "../spacer/spacer.component";
 import { CompressRestaurantInfo } from "../restaurant/compress-restaurant-info.component";
+import { Text } from "../typography/text.component";
 
 const FavouritesWrapper = styled.View`
   padding: 10px;
   border-radius: 5px;
 `;
-export const FavouritesBar = ({ favourites, onNavigate }) => (
-  <FavouritesWrapper>
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {favourites.map((restaurant) => {
-        const key = restaurant.name;
-        return (
-          <TouchableOpacity
-            onPress={() => {
-              onNavigate("Restaurantdetails", {
-                restaurant,
-              });
-            }}
-          >
-            <Spacer key={key} position="left" size="medium">
-              <CompressRestaurantInfo restaurant={restaurant} />
-            </Spacer>
-          </TouchableOpacity>
-        );
-      })}
-    </ScrollView>
-  </FavouritesWrapper>
-);
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (favourites.length === 0) {
+    return null;
+  }
+  return (
+    <FavouritesWrapper>
+      <Spacer variant="left.medium">
+        <Text variant="caption">Your Favorites</Text>
+      </Spacer>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {favourites.map((restaurant) => {
+          const key = restaurant.name;
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                onNavigate("Restaurantdetails", {
+                  restaurant,
+                });
+              }}
+            >
+              <Spacer key={key} position="left" size="medium">
+                <CompressRestaurantInfo restaurant={restaurant} />
+              </Spacer>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </FavouritesWrapper>
+  );
+};
