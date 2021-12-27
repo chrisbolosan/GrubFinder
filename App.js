6ff35471252ec03e1d3f4053cdfb4ed59f080440
chrisbolosan <cbolosan@gmail.com>
2021-12-26 21:57:32 -0600

    firebase api connected and authorization api works

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { RestaurantsContextProvider } from "./src/services/restaurant/restaurants.context.js";
 import { LocationContextProvider } from "./src/services/location/location.context.js";
 import { Navigation } from "./src/infrastructure/navigation/index.js";
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { firebaseConfig } from "./firebase.js";
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -13,11 +17,25 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context.js";
 
-const firebaseConfig = process.env.firebaseConfiguration;
-
-initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("foodcritic@gmail.com", "Password123*")
+      .then((user) => {
+        console.log("fireusertest", user);
+        setIsAuthenticated(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -26,6 +44,9 @@ export default function App() {
   });
 
   if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+  if (!isAuthenticated) {
     return null;
   }
   return (
