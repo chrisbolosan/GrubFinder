2591791bb850c3ea237a07209ecadd3552f9eaba
chrisbolosan <cbolosan@gmail.com>
2021-12-28 17:53:52 -0600

    new background for accout UX

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,10 +1,10 @@
 import styled from "styled-components/native";
 
 export const AccountBackground = styled.ImageBackground.attrs({
-  source: require("../../../../assets/foodbackground.jpeg"),
+  source: require("../../../../assets/foodbackground3.jpeg"),
+  //   resizeMode: "contain",
 })`
   flex: 1;
   align-items: center;
   justify-content: center;
-  resizeMode=cover;
 `;
