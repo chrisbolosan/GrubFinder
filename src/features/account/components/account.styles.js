9846b290b481a7cb65423cf8ba0997534fbf90ef
chrisbolosan <cbolosan@gmail.com>
2021-12-29 16:57:46 -0600

    container for buttons

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -15,3 +15,9 @@ export const AccountCover = styled.View`
   height: 100%;
   background-color: rgba(255, 255, 255, 0.2);
 `;
+
+export const AccountContainer = styled.View`
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: ${(props) => props.theme.space[5]};
+  margin-top: ${(props) => props.theme.space[3]};
+`;
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Text } from "react-native";
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import styled from "styled-components";
+import { fontWeights } from "../../../infrastructure/theme/fonts";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+} from "../components/account.styles";
 
+const chrisfood = styled.Text`
+  color: rgba(255, 255, 255);
+`;
 export const AccountScreen = () => {
   return (
     <>
       <AccountBackground>
-        <Text>main</Text>
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: "25px" }}>
+          GrubFinder
+        </Text>
+        <AccountContainer />
         <AccountCover />
       </AccountBackground>
     </>
