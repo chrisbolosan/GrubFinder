69e4ad0d7c4b1f5cdb3c34934adb8e44d5970d11
chrisbolosan <cbolosan@gmail.com>
2021-12-29 16:28:50 -0600

    better opacity

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -13,5 +13,5 @@ export const AccountCover = styled.View`
   position: absolute;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.3);
+  background-color: rgba(255, 255, 255, 0.2);
 `;
