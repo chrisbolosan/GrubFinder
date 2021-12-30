4c27408c0e34b3985d9fbdf3791e6976d483cf03
chrisbolosan <cbolosan@gmail.com>
2021-12-30 17:54:36 -0600

    more error styling

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -45,4 +45,7 @@ export const AuthInput = styled(TextInput)`
 export const ErrorContainer = styled.View`
   max-width: 300px;
   align-items: center;
+  align-self: center;
+  margin-top: ${(props) => props.theme.space[2]};
+  margin-bottom: ${(props) => props.theme.space[1]};
 `;
