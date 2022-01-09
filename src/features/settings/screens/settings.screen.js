f8859957476c74ca91097bb3f0c08b42f7c06e5c
chrisbolosan <cbolosan@gmail.com>
2022-01-09 14:08:35 -0600

    screen stacking settings

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,12 +1,27 @@
 import React, { useContext } from "react";
 import { Button } from "react-native";
+import { List } from "react-native-paper";
 import { SafeArea } from "../../../components/utils/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-export const SettingsScreen = () => {
+export const SettingsScreen = ({ navigation }) => {
   const { onLogout } = useContext(AuthenticationContext);
   return (
     <SafeArea>
+      <List.Section>
+        <List.Subheader>Settings</List.Subheader>
+        <List.Item
+          title="Favorites"
+          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
+          onPress={() => navigation.navigate("Favourites")}
+        />
+        <List.Item
+          title="Logout"
+          left={(props) => <List.Icon {...props} color="#000" icon="folder" />}
+          onPress={() => navigation.navigate("logout")}
+        />
+      </List.Section>
+
       {/* <Text>Settings!</Text> */}
       <Button title="logout" onPress={() => onLogout()}>
         Logout
